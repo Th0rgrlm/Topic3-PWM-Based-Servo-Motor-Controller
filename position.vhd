a6f028197ff4b69433860450eb4f2d06f990bef3
Th0rgrlm <153467129+Th0rgrlm@users.noreply.github.com>
2024-04-03 13:54:55 +0200

    Update position.vhd

--- position.vhd
+++ position.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.math_real.all;
+use ieee.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -43,10 +45,27 @@ end position;
 
 architecture Behavioral of position is
 
---counter from 100 to 200 up using BTNR down using BTNL
---BTNC sets pos to 150
+signal sig_count : std_logic_vector(7 downto 0);
 
 begin
-
-
-end Behavioral;
+    p_clk_enable : process (clk) is
+    begin
+        if (rising_edge(clk)) then
+            if rst = '1' then
+                sig_count <= b"1001_0110";
+            elsif en = '1' then
+                if left ='1' and sig_count>=b"0110_0100" then
+                    sig_count <= sig_count - 1;
+                end if;
+                if right = '1' and sig_count<=b"1100_1000" then
+                    sig_count <= sig_count + 1;
+                end if;
+            end if;
+        end if;
+    end process p_clk_enable;
+    
+    pos <= sig_count;  
+    
+--counter from 100 to 200 up using BTNR down using BTNL
+--BTNC sets pos to 150
+end;
